5bb8b166a44a11a43587b09d7a03ebc2377bd326
Shikilion <168650529+Shikilion@users.noreply.github.com>
2024-08-06 15:15:18 +0700

    Add files via upload

---
+++ Final Project Of Tkinter/song.py
@@ -0,0 +1,9 @@
+class Song:
+    def __init__(self, title, singer, genre, duration):
+        self.title = title
+        self.singer = singer
+        self.genre = genre
+        self.duration = duration
+
+
+        
