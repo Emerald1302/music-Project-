1812bde331cfd68209723373d17d4a0768dbd421
Emerald1302 <168625404+Emerald1302@users.noreply.github.com>
2024-08-08 19:29:00 +0700

    Delete song.py

--- song.py
+++
@@ -1,9 +0,0 @@
-class Song:
-    def __init__(self, title, singer, genre, duration):
-        self.title = title
-        self.singer = singer
-        self.genre = genre
-        self.duration = duration
-
-
-        
